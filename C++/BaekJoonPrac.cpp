d3040a4647b10e494d0812b4ee4d59c709b3d280
Junhyun <thesimpson33@gmail.com>
2023-01-18 14:42:13 +0900

    Add folder

---
+++ C++/BaekJoonPrac.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int iNum = 0;
+
+int recursion(const char* s, int l, int r){
+    iNum++;
+    if(l>=r){
+        return 1;
+    }
+    else if(s[l] != s[r]){
+        return 0;
+    }
+    else{
+        return recursion(s,l+1, r-1);
+    }
+}
+
+int isPalindrome(const char* s){
+    return recursion(s, 0, strlen(s)-1);
+}
+
+int main(){
+    int iTmp;
+    string sInput;
+    cin>>iTmp;
+    for(int i =0; i<iTmp; i++){
+        cin>>sInput;
+        cout<<isPalindrome(sInput.c_str())<<" "<<iNum<<"\n";
+        iNum=0;
+    }
+}
